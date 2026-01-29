8b1f1a6fcb262db805e161ee4ade79f41d8e2b9c
rayhanalmim <rayhanalmim1@gmail.com>
2026-01-30 01:46:15 +0600

    updated

--- src/lib/web3Config.ts
+++ src/lib/web3Config.ts
@@ -26,8 +26,8 @@ export const CHAIN_CONFIG = {
     rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'],
     blockExplorerUrls: ['https://testnet.bscscan.com'],
     contracts: {
-      voterNFT: '0x02e2cc3f51b2d8fa98fcea62bdbb126f3e29dd94',
-      votingSystem: '0xaa765eb225823131a8a9f608eed7d2bb0cbd7248',
+      voterNFT: '0x51f1182d716b119e05e07643843f874ad04874c0',
+      votingSystem: '0xd79a6599ab5fef53453744ebb2cc9d8e234f2b4f',
     },
   },
   [CHAIN_IDS.ETH_SEPOLIA]: {
